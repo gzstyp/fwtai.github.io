a424b7c7a99a7545197f4e6014aff9201fc7f3fc
引路者 <444141300@qq.com>
2021-05-19 10:31:04 +0800

    更新内容-替换首页

---
+++ js/page.common.js
@@ -0,0 +1,17 @@
+/**
+ * @作者 田应平
+ * @创建时间 2019-03-26 2:37
+ * @QQ号码 444141300
+ * @官网 http://www.fwtai.com
+ */
+;(function($){
+    window.thisPage = {
+        init : function(){
+        },
+        initDom : function(){
+        },
+        addEvent : function(){
+        }
+    };
+    thisPage.init();
+})(jQuery);
